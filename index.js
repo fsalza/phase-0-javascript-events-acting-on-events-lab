e86bb3de58605d4fa61567b653da0edb872c4004
Fulvio Salza <fsalza96@gmail.com>
2023-04-04 10:01:39 -0400

    complete

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 // Your code here
-const dodger = document.getElementById("dodger");
+const dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
-  const leftNumbers = dodger.style.left.replace("px", "");
+  const leftNumbers = dodger.style.left.replace('px', ' ');
   const left = parseInt(leftNumbers, 10);
 
-  if (left > 0) {
+  if (left > 0){
     dodger.style.left = `${left - 1}px`;
   }
 }
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
-  }
-});
 
-function moveDodgerRight() {
-  const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
-
-  if (left > 0) {
+function moveDodgerRight(){
+  const rightNumbers = dodger.style.left.replace('px', ' ');
+  const left = parseInt(rightNumbers, 10);
+  if (left > 0){
     dodger.style.left = `${left + 1}px`;
   }
 }
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowRight") {
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight"){
     moveDodgerRight();
   }
-});
+})
+
